e643e2bd1f79d8f7a9f3e969d9b4bb11758ceec2
adrpadua <adrpadua@gmail.com>
2016-12-21 19:35:27 -0800

    Added weapon and armor

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var collection = [];
 
-fs.readFile('./5e-SRD-classtables.json', 'utf8', function (err,data) {
+fs.readFile('./5e-SRD-equipment.json', 'utf8', function (err,data) {
   data = JSON.parse(data);
 
 	var keys = Object.keys(data);
@@ -10,14 +10,14 @@ fs.readFile('./5e-SRD-classtables.json', 'utf8', function (err,data) {
 	// number the indexes
 	for(var i = 0; i < data.length; i++) {
 		data[i].index = i + 1;
-		data[i].url = "http://dnd5eapi.co/api/classtables/"+ (i + 1).toString();
+		data[i].url = "http://dnd5eapi.co/api/equipment/"+ (i + 1).toString();
 		collection.push(data[i]);
 	}
 
-	fs.writeFile('./5e-SRD-classtables-upload.json', JSON.stringify(collection), (err) => {
+	fs.writeFile('./5e-SRD-equipment-upload.json', JSON.stringify(collection), (err) => {
 		if (err) throw err;
 		console.log('Success');
   	});
 });
 
-// mongoimport -h ds133158.mlab.com:33158 -d 5e-srd-api -c classtables -u admin -p password --file 5e-SRD-classtables-upload.json --jsonArray
+// mongoimport -h ds133158.mlab.com:33158 -d 5e-srd-api -c equipment -u admin -p password --file 5e-SRD-equipment-upload.json --jsonArray
