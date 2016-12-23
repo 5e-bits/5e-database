2c14ada0020632b0b11da75c51ce3fce24e883d5
adrpadua <adrpadua@gmail.com>
2016-12-22 17:37:06 -0800

    better helper file

--- helper.js
+++ helper.js
@@ -1,23 +1,40 @@
 var fs = require('fs');
+let all_data_types = ["classes", "classfeatures", "classtables", "equipment", "monsters", "proficiencies", "spells", "startingequipment"]
 
-var collection = [];
+function create_upload_file(datatype_string) {
 
-fs.readFile('./5e-SRD-spells.json', 'utf8', function (err,data) {
-  data = JSON.parse(data);
+	let input_filename = "./5e-SRD-" + datatype_string + ".json"
+	let index_and_urlify = (err,data) => {
+		
+		data = JSON.parse(data);
 
-	var keys = Object.keys(data);
+		let keys = Object.keys(data);
 
-	// number the indexes
-	for(var i = 0; i < data.length; i++) {
-		data[i].index = i + 1;
-		data[i].url = "http://dnd5eapi.co/api/spells/"+ (i + 1).toString();
-		collection.push(data[i]);
+		// number the indexes
+		for(let i = 0; i < data.length; i++) {
+			data[i].index = i + 1;
+			data[i].url = "http://dnd5eapi.co/api/" + datatype_string + "/"+ (i + 1).toString();
+		}
+
+		let output_filename = "./upload-5e-SRD-" + datatype_string + ".json";
+
+		fs.writeFile(output_filename, JSON.stringify(data), (err) => {
+			if (err) throw err;
+			console.log('Success');
+			console.log("mongoimport -h ds133158.mlab.com:33158 -d 5e-srd-api -c " + datatype_string + " -u admin -p password --file upload-5e-SRD-" + datatype_string + ".json --jsonArray")
+		});
 	}
 
-	fs.writeFile('./5e-SRD-spells-upload.json', JSON.stringify(collection), (err) => {
-		if (err) throw err;
-		console.log('Success');
-  	});
-});
+	fs.readFile(input_filename, 'utf8', (err,data) => {
+		return index_and_urlify(err,data);
+	})
 
-// mongoimport -h ds133158.mlab.com:33158 -d 5e-srd-api -c spells -u admin -p password --file 5e-SRD-spells-upload.json --jsonArray
+}
+
+create_upload_file('spells');
+
+for (let i = 0; i < all_data_types.length; i++) {
+	create_upload_file(all_data_types[i]);
+}
+
+// mongoimport -h ds133158.mlab.com:33158 -d 5e-srd-api -c classfeatures -u admin -p password --file upload-5e-SRD-classfeatures.json --jsonArray
