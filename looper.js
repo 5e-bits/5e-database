201a9df380532e2fec3abd11d9602f08504badb1
adrpadua <adrpadua@gmail.com>
2016-12-24 18:10:52 -0800

    Added optioned features

--- looper.js
+++ looper.js
@@ -1,3 +1,7 @@
-for (var i = 0; i < 36; i++) {
-    console.log("\"http://dnd5eapi.co/api/features/" + (275 + i) + "\",");
+const args = process.argv;
+let start_index = args[2]
+let num_items = args[3]
+
+for (var i = 0; i < num_items; i++) {
+    console.log("\"http://dnd5eapi.co/api/features/" + (parseInt(start_index) + i) + "\",");
 }
