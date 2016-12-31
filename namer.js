38181789e3e8a217bce87dab5fe1a0abb3aa7675
adrpadua <adrpadua@gmail.com>
2016-12-30 17:14:39 -0800

    added conditions, damagetypes, and magic schools. updated some files to new format

---
+++ namer.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+const args = process.argv;
+let resource = args[2]
+let start_index = args[3]
+let end_index = args[4]
+
+let input_filename = "./5e-SRD-" + resource + ".json"
+
+fs.readFile(input_filename, 'utf8', (err,data) => {
+    data = JSON.parse(data);		
+    var i = start_index - 1;
+    while (i != (parseInt(end_index))) {
+        console.log(data[i].name);
+        i++;
+    }
+})
