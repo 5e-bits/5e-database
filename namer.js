1e8ed6663dfdad6cff15e4f277daaf7aefd3193f
Chris Ward <cdurianward@gmail.com>
2019-04-12 13:49:15 -0700

    Fixing some spacing and indentations. (#62)

--- namer.js
+++ namer.js
@@ -8,10 +8,10 @@ let end_index = args[4]
 let input_filename = "./5e-SRD-" + resource + ".json"
 
 fs.readFile(input_filename, 'utf8', (err,data) => {
-    data = JSON.parse(data);		
-    var i = start_index - 1;
-    while (i != (parseInt(end_index))) {
-        console.log(data[i].name);
-        i++;
-    }
+  data = JSON.parse(data);
+  var i = start_index - 1;
+  while (i != (parseInt(end_index))) {
+    console.log(data[i].name);
+    i++;
+  }
 })
