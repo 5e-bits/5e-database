c9a03834c4583d740000798a732da9e03c6de96a
Mikkel Paulson <git@email.mikkel.ca>
2021-07-30 20:55:21 -0400

    add test to verify integrity of internal links
    * checks that outgoing links are defined as top-level resources
    * checks that name fields match (if defined)
    * checks that index fields match (if defined)
    
    Test is currently failing and marked as skipped.

--- src/tests/tables.test.js
+++ src/tests/tables.test.js
@@ -20,3 +20,60 @@ describe("duplicate indices", () => {
     expect(errors).toEqual([]);
   });
 });
+
+describe.skip("broken links", () => {
+  it("should not contain broken links", () => {
+    let errors = [];
+    let files = glob.sync("src/*.json");
+
+    let resources = {};
+
+    const walk_recursive = (resources, file, object, errors) => {
+      for (const property in object) {
+        if (property === "url") {
+          if (resources[object.url] === undefined) {
+            errors.push(`${file}: URL '${object.url}' not found.`);
+          } else {
+            if (resources[object.url].name !== undefined && resources[object.url].name !== object.name) {
+              errors.push(`${file}: Name mismatch for reference to '${object.url}', '${object.name}' should be '${resources[object.url].name}'`);
+            }
+
+            if (object.index !== undefined && resources[object.url].index !== object.index) {
+              errors.push(`${file}: Index mismatch for reference to '${object.url}', '${object.index}' should be '${resources[object.url].index}'`);
+            }
+          }
+        } else if (typeof object[property] === "object") {
+          walk_recursive(resources, file, object[property], errors);
+        }
+      }
+    };
+
+    for (const file of files) {
+      const fileText = fs.readFileSync(file, "utf8");
+      const fileJSON = JSON.parse(fileText);
+      fileJSON.forEach((entry) => {
+        if (entry.url !== undefined) {
+          if (entry.index === undefined) {
+            errors.push(`${file}: Entity with URL '${entry.url}' should have an index.`);
+          }
+
+          resources[entry.url] = { index: entry.index, name: entry.name };
+        }
+      });
+    }
+
+    for (const file of files) {
+      const fileText = fs.readFileSync(file, "utf8");
+      const fileJSON = JSON.parse(fileText);
+      fileJSON.forEach((entry) => {
+        for (const property in entry) {
+          if (typeof entry[property] === "object") {
+            walk_recursive(resources, file, entry[property], errors);
+          }
+        }
+      });
+    }
+
+    expect(errors).toEqual([]);
+  });
+});
