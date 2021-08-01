e12c1e2d9d3dc339d78ad6aeb39221a7ecc252dd
Mikkel Paulson <git@email.mikkel.ca>
2021-08-01 11:18:15 -0400

    simplify for loop in walk_recursive

--- src/tests/tables.test.js
+++ src/tests/tables.test.js
@@ -29,22 +29,24 @@ describe("broken links", () => {
 
     let resources = {};
 
-    const walk_recursive = (resources, file, object, errors) => {
-      for (const property in object) {
-        if (property === "url") {
-          if (resources[object.url] === undefined) {
-            errors.push(`${file}: URL '${object.url}' not found.`);
-          } else {
-            if (resources[object.url].name !== undefined && resources[object.url].name !== object.name) {
-              errors.push(`${file}: Name mismatch for reference to '${object.url}', '${object.name}' should be '${resources[object.url].name}'`);
-            }
-
-            if (object.index !== undefined && resources[object.url].index !== object.index) {
-              errors.push(`${file}: Index mismatch for reference to '${object.url}', '${object.index}' should be '${resources[object.url].index}'`);
-            }
+    const walk_recursive = (resources, file, entry, errors) => {
+      if (entry.hasOwnProperty("url")) {
+        if (resources[entry.url] === undefined) {
+          errors.push(`${file}: URL '${entry.url}' not found.`);
+        } else {
+          if (resources[entry.url].name !== undefined && resources[entry.url].name !== entry.name) {
+            errors.push(`${file}: Name mismatch for reference to '${entry.url}', '${entry.name}' should be '${resources[entry.url].name}'`);
           }
-        } else if (typeof object[property] === "object") {
-          walk_recursive(resources, file, object[property], errors);
+
+          if (entry.index !== undefined && resources[entry.url].index !== entry.index) {
+            errors.push(`${file}: Index mismatch for reference to '${entry.url}', '${entry.index}' should be '${resources[entry.url].index}'`);
+          }
+        }
+      }
+
+      for (const property in entry) {
+        if (typeof entry[property] === "object" && entry[property] !== null) {
+          walk_recursive(resources, file, entry[property], errors);
         }
       }
     };
@@ -61,7 +63,7 @@ describe("broken links", () => {
 
     forEachFile((filename, entry) => {
       for (const property in entry) {
-        if (typeof entry[property] === "object") {
+        if (typeof entry[property] === "object" && entry[property] !== null) {
           walk_recursive(resources, filename, entry[property], errors);
         }
       }
