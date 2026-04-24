9bf45e89eb7fcc43bfc0135253a7d13a84f1f41f
Chris Ward <cdurianward@gmail.com>
2026-04-24 11:05:28 -0700

    fix(2024): Change field names for primary ability in classes and convert to choice schema (#1091)
    ## What does this do?
    
    * Swaps a few fields on classes:
      * `all_of` -> `ability_scores`
      * `any_of` -> `ability_score_options`
    * `ability_score_options` now uses the `Choice` schema
    * Class schema now uses `strictObject`
    
    ## Here's a fun image for your troubles
    
    <img width="686" height="386" alt="image"
    src="https://github.com/user-attachments/assets/62626e0f-d456-411e-9afd-0c19197554d9"
    />

--- src/2024/schemas/5e-SRD-Classes.ts
+++ src/2024/schemas/5e-SRD-Classes.ts
@@ -1,36 +1,36 @@
 import { z } from 'zod';
 import { APIReferenceSchema, ChoiceSchema } from '../../schemas/common';
 
-const SpellcastingInfoSchema = z.object({
+const SpellcastingInfoSchema = z.strictObject({
   name: z.string(),
   desc: z.array(z.string()),
 });
 
-const SpellcastingSchema = z.object({
+const SpellcastingSchema = z.strictObject({
   level: z.number(),
   spellcasting_ability: APIReferenceSchema,
   info: z.array(SpellcastingInfoSchema),
 });
 
-const MultiClassingPrereqSchema = z.object({
+const MultiClassingPrereqSchema = z.strictObject({
   ability_score: APIReferenceSchema.optional(),
   minimum_score: z.number(),
 });
 
-const MultiClassingSchema = z.object({
+const MultiClassingSchema = z.strictObject({
   prerequisites: z.array(MultiClassingPrereqSchema).optional(),
   prerequisite_options: ChoiceSchema.optional(),
   proficiencies: z.array(APIReferenceSchema).optional(),
   proficiency_choices: z.array(ChoiceSchema).optional(),
 });
 
-const PrimaryAbilitySchema = z.object({
+const PrimaryAbilitySchema = z.strictObject({
   desc: z.string(),
-  all_of: z.array(APIReferenceSchema).optional(),
-  any_of: ChoiceSchema.optional()
+  ability_scores: z.array(APIReferenceSchema).optional(),
+  ability_score_options: ChoiceSchema.optional(),
 });
 
-export const ClassSchema = z.object({
+export const ClassSchema = z.strictObject({
   index: z.string(),
   name: z.string(),
   primary_ability: PrimaryAbilitySchema,
