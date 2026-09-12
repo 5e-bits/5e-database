b910e6981ce5de122738b331156954b6781b583e
G.Ambatte <sean@robertson-family.nz>
2026-09-13 05:19:25 +1200

    feat(2024): Add 2024 Spells (#1178)
    ## What does this do?
    
    This PR adds the 2024 Spell data.
    
    ## How was it tested?
    
    Locally
    
    ## Is there a Github issue this is resolving?
    
    No
    
    ## Did you update the docs in the API? Please link an associated PR if
    applicable.
    
    No
    
    ## Here's a fun image for your troubles
    
    <img width="809" height="1080" alt="image"
    src="https://github.com/user-attachments/assets/89bd8ddf-5f29-40c4-bb24-051e452635aa"
    />

---
+++ src/2024/schemas/5e-SRD-Spells.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+import { APIReferenceSchema, AreaOfEffectSchema } from '../../schemas/common';
+
+const SpellDamageSchema = z.strictObject({
+  damage_type: APIReferenceSchema.optional(),
+  damage_at_slot_level: z.record(z.string(), z.string()).optional(),
+  damage_at_character_level: z.record(z.string(), z.string()).optional(),
+});
+
+const SpellDCSchema = z.strictObject({
+  dc_type: APIReferenceSchema,
+  dc_success: z.string(),
+  desc: z.string().optional(),
+});
+
+export const SpellsSchema = z.strictObject({
+  index: z.string(),
+  name: z.string(),
+  description: z.string(),
+  higher_level: z.string().optional(),
+  range: z.string(),
+  components: z.array(z.string()),
+  material: z.string().optional(),
+  ritual: z.boolean(),
+  duration: z.string(),
+  concentration: z.boolean(),
+  casting_time: z.string(),
+  level: z.number(),
+  attack_type: z.string().optional(),
+  damage: SpellDamageSchema.optional(),
+  dc: SpellDCSchema.optional(),
+  area_of_effect: AreaOfEffectSchema.optional(),
+  heal_at_slot_level: z.record(z.string(), z.string()).optional(),
+  school: APIReferenceSchema,
+  classes: z.array(APIReferenceSchema),
+  subclasses: z.array(APIReferenceSchema).optional(),
+  url: z.string(),
+});
